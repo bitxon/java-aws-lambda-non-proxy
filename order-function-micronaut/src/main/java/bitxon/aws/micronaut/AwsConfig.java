98e0f3486f7ba97bd1ae0ac5d6e12f62a94b42ad
Nikita Karpuk <bitxon.dev@gmail.com>
2025-11-30 23:06:37 +0100

    Micronaut 4.10.2

--- order-function-micronaut/src/main/java/bitxon/aws/micronaut/AwsConfig.java
+++ order-function-micronaut/src/main/java/bitxon/aws/micronaut/AwsConfig.java
@@ -1,14 +1,9 @@
 package bitxon.aws.micronaut;
 
 import io.micronaut.context.annotation.Factory;
-import jakarta.inject.Singleton;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
 
 @Factory
 public class AwsConfig {
-
-    @Singleton
-    public DynamoDbClient dynamoDbClient() {
-        return DynamoDbClient.builder().build();
-    }
+    // DynamoDbClient is now provided automatically by micronaut-aws-sdk-v2
 }
